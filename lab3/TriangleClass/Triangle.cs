981c1caf22ca48fe18826e51424a72d59f305343
Pavel <pavel.bareyko@mail.ru>
2019-10-01 17:16:17 +0300

    replacing >= with >

--- lab3/TriangleClass/Triangle.cs
+++ lab3/TriangleClass/Triangle.cs
@@ -9,6 +9,6 @@ namespace TriangleLib
     public class Triangle
     {
         public static bool IsTriangle(double a, double b, double c) => 
-            (a > 0 && b > 0 && c > 0 && (a + b >= c) && (a + c >= b) && (b + c >= a));
+            (a > 0 && b > 0 && c > 0 && (a + b > c) && (a + c > b) && (b + c > a));
     }
 }
