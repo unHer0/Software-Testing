a6efc51348ec5f16f67ba41d674779a5f6e17d1a
Pavel <pavel.bareyko@mail.ru>
2019-10-01 13:53:40 +0300

    add unittests

---
+++ lab3/TriangleClass/Triangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleLib
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(double a, double b, double c) => 
+            (a > 0 && b > 0 && c > 0 && (a + b >= c) && (a + c >= b) && (b + c >= a));
+    }
+}
---
+++ lab3/TriangleLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriangleLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriangleLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f966f10-a759-453f-a7a3-8641b396d8a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
